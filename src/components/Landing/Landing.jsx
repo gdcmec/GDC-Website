41a9cb8a8c071522ac8a2cd0e2cc57e2f1061343
Devadutt <devaduttmohancs@gmail.com>
2024-10-16 22:57:32 +0530

    Feat: Added Distort effect to title

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -44,7 +44,8 @@ const landing = () => {
                whileInView={{opacity: 1, y: 0}}
                transition={{duration: 1}}
                className='landing-h1'
-              >Game Development<br></br>Club<br></br></motion.h1>
+               data-text = "Game Development">
+              <h1 className='landing-h1'>Game Development<br></br>Club<br></br></h1></motion.h1>
               <motion.span
               id = "sub"
               initial={{ opacity: 0, y: -50}}
